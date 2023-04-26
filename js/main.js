470b05b7476c2790f63799ba17d208df7b654827
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-26 15:37:00 +0200

    need help

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const griglia = document.getElementById("grid");
+const play = document.getElementById("startGame")
+
+// Griglia 10x10 = 100 elementi
+
+//  function griglia(gridPieces) {
+    for (let i = 0; i < 100; i++) {
+    
+        const nuovoDiv = document.createElement("div");
+
+        // nuovoDiv.classList.add(gridPieces);
+        nuovoDiv.classList.add("boxPiecesX100");
+    
+        nuovoDiv.addEventListener("click", function() {
+            console.log("this: ", this);
+            this.classList.toggle("clicked");
+        });
+    
+        griglia.appendChild(nuovoDiv);
+    }
+// }
+
+// play.addEventListener("click", function() {
+//     griglia(boxPiecesX100)
+// })
+
