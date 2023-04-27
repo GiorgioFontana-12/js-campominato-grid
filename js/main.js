57e6cb60696c6ce761a961bc3bd6bda8b0a0f3cb
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-27 12:00:19 +0200

    clean code

--- js/main.js
+++ js/main.js
@@ -2,31 +2,23 @@ const griglia = document.getElementById("grid");
 const play = document.getElementById("startGame")
 const difficulty = document.getElementById("level")
 
-// Griglia 10x10 = 100 elementi
-
  function sqareGen(gridPieces, totalPices) {
     griglia.innerHTML = "";
     for (let i = 0; i < totalPices; i++) {
-        
         const nuovoDiv = document.createElement("div");
 
         nuovoDiv.classList.add(gridPieces);
-        // nuovoDiv.classList.add("boxPiecesX100");
-    
         nuovoDiv.addEventListener("click", function() {
             console.log("this: ", this);
             this.classList.toggle("clicked");
         });
-    
         griglia.appendChild(nuovoDiv);
         nuovoDiv.innerHTML = i+1
     }
 }
-
 play.addEventListener("click", function() {
     var difficultySelected = difficulty.options[difficulty.selectedIndex].value;
     console.log('You selected: ', difficultySelected);
 
     sqareGen(`boxPiecesX${difficultySelected}` , difficultySelected)
 })
-
